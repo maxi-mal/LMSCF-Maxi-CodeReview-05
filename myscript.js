c0118483481e7dd05245f701c7e2d85f0be9fc9f
maxi-mal <57346800+maxi-mal@users.noreply.github.com>
2020-02-14 16:46:09 +0100

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,19 @@
+var movies = JSON.parse(movies);
+console.log(movies);
+
+$(document).ready(function(){
+	for (var i = 0; i < movies.length; i++) {
+		$("#background").append(
+			`<div class="col-5" id="moviebox${movies[i].uniqueid}">
+			<div id="imgbox">
+			${movies[i].image} style="height:300px">
+			</div>
+			<div id="textbox">
+				<p id="movietitle">${movies[i].title}</p>
+				<p id="moviedescrip">${movies[i].description}</p>
+				<div id="rating">${movies[i].rating}></div>
+			</div>
+		</div>`
+		)
+	}
+});
