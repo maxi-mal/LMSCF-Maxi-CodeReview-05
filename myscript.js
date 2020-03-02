011f974b7f440f7252a46977a5dd28035cd065f0
maxi-mal <57346800+maxi-mal@users.noreply.github.com>
2020-03-02 02:16:15 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,19 +1,42 @@
 var movies = JSON.parse(movies);
 console.log(movies);
 
-$(document).ready(function(){
-	for (var i = 0; i < movies.length; i++) {
-		$("#background").append(
-			`<div class="col-5" class="col-5${movies[i].uniqueid}">
-			<div id="imgbox">
-			${movies[i].image} style="height:300px">
-			</div>
-			<div id="textbox">
-				<p id="movietitle">${movies[i].title}</p>
-				<p id="moviedescrip">${movies[i].description}</p>
-				<div id="rating">${movies[i].rating}></div>
-			</div>
-		</div>`
-		)
-	}
+$(document).ready(function() {
+    for (var i = 0; i < movies.length; i++) {
+        $("#background").append(
+            `<div class="col-5" class="col-5${movies[i].uniqueid}">
+				<div class="col-5" id="imgbox">${movies[i].image} style="height:300px">
+				</div>
+					<div class="col-7" id="textbox">
+						<p id="movietitle">${movies[i].title}</p>
+						<p id="moviedescrip">${movies[i].description}</p>
+							<div class="Likes">
+						  		<div id="like">
+						  			Like
+						  		</div>
+								<div id="thumb">
+					  				<button id="clickme">
+					  				<img src="img/greenthumb.png">
+					  				</button
+					  			</div>
+					  			<div class="counter">
+					  			</div>
+						  	</div>				
+					</div>
+			</div>`
+        )
+    }
+
+    var counter = $("button");
+    var newValue = $(".counter");
+
+    for (let i = 0; i < movies.length; i++) {
+
+        $(counter[i]).on('click', function() {
+            ++movies[i].like;
+            $(newValue[i]).text(movies[i].like);
+        });
+    }
+
+
 });
